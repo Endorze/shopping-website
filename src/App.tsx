528b36884c2ed9218e14b4af12b19ef10517f9f5
Endorze <alexyurihallgren@hotmail.com>
2025-03-26 17:22:49 +0100

    feat: Added category section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import Header from './components/Header/header'
+import styles from "./components/CategoryTile/categoryTile.module.css"
+import { categoryInfo } from './data/clothes'
+import CategoryTile from './components/CategoryTile/categoryTile'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <div className={styles.categoryTileContainer}>
+        {categoryInfo && categoryInfo.map((item, index) => (
+          <CategoryTile key={index} {...item} />)
+        )}
+      </div>
     </>
   )
 }
